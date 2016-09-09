7fe7fdd2786f5eddf1c81a72b4af1cb9174ef976
khmarbaise <khmarbaise@apache.org>
2016-09-09 22:25:45 +0200

    Improved and refactored code.

--- src/main/java/com/soebes/maven/extensions/deployer/MavenDeployer.java
+++ src/main/java/com/soebes/maven/extensions/deployer/MavenDeployer.java
@@ -1,15 +1,11 @@
 package com.soebes.maven.extensions.deployer;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.eventspy.AbstractEventSpy;
 import org.apache.maven.execution.ExecutionEvent;
@@ -17,9 +13,6 @@ import org.apache.maven.execution.ExecutionEvent.Type;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.model.PluginExecution;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.artifact.ProjectArtifactMetadata;
-import org.apache.maven.shared.artifact.deploy.ArtifactDeployer;
-import org.apache.maven.shared.artifact.deploy.ArtifactDeployerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,7 +27,7 @@ public class MavenDeployer
     private final Logger LOGGER = LoggerFactory.getLogger( getClass() );
 
     @Inject
-    private ArtifactDeployer deployer;
+    private DeployProject deployProject;
 
     public MavenDeployer()
     {
@@ -180,140 +173,18 @@ public class MavenDeployer
     private void deployArtifacts( ExecutionEvent executionEvent )
     {
         List<MavenProject> sortedProjects = executionEvent.getSession().getProjectDependencyGraph().getSortedProjects();
+
+        ArtifactRepository distributionManagementArtifactRepository =
+            executionEvent.getSession().getTopLevelProject().getDistributionManagementArtifactRepository();
         for ( MavenProject mavenProject : sortedProjects )
         {
             LOGGER.info( "Deploying artifact: " + mavenProject.getId() );
-            DeployRequest currentExecutionDeployRequest =
-                new DeployRequest().setProject( mavenProject ).setUpdateReleaseInfo( true );
+            DeployRequest deployRequest = new DeployRequest().setProject( mavenProject ).setUpdateReleaseInfo( true );
 
-            deployProject( executionEvent, currentExecutionDeployRequest );
+            // distributionManagementArtifactRepository
 
-        }
-    }
-
-    private void deployProject( ExecutionEvent executionEvent, DeployRequest request )
-    {
-        List<Artifact> deployableArtifacts = new ArrayList<Artifact>();
-
-        Artifact artifact = request.getProject().getArtifact();
-        String packaging = request.getProject().getPackaging();
-        File pomFile = request.getProject().getFile();
-
-        List<Artifact> attachedArtifacts = request.getProject().getAttachedArtifacts();
-
-        ArtifactRepository repo = request.getProject().getDistributionManagementArtifactRepository();
-
-        LOGGER.debug( "Deployment repo:" + repo );
-        // Deploy the POM
-        boolean isPomArtifact = "pom".equals( packaging );
-        if ( !isPomArtifact )
-        {
-            ProjectArtifactMetadata metadata = new ProjectArtifactMetadata( artifact, pomFile );
-            artifact.addMetadata( metadata );
-        }
-        else
-        {
-            artifact.setFile( pomFile );
-        }
-
-        if ( request.isUpdateReleaseInfo() )
-        {
-            artifact.setRelease( true );
-        }
-
-        artifact.setRepository( repo );
-
-        int retryFailedDeploymentCount = request.getRetryFailedDeploymentCount();
-
-        try
-        {
-            if ( isPomArtifact )
-            {
-                deployableArtifacts.add( artifact );
-            }
-            else
-            {
-                File file = artifact.getFile();
-
-                if ( file != null && file.isFile() )
-                {
-                    deployableArtifacts.add( artifact );
-                }
-                else if ( !attachedArtifacts.isEmpty() )
-                {
-                    throw new IllegalArgumentException( "The packaging plugin for this project did not assign "
-                        + "a main file to the project but it has attachments. Change packaging to 'pom'." );
-                }
-                else
-                {
-                    throw new IllegalArgumentException( "The packaging for this project did not assign "
-                        + "a file to the build artifact" );
-                }
-            }
-
-            for ( Artifact attached : attachedArtifacts )
-            {
-                // This is here when AttachedArtifact is used, like m-sources-plugin:2.0.4
-                try
-                {
-                    attached.setRepository( repo );
-                }
-                catch ( UnsupportedOperationException e )
-                {
-                    LOGGER.warn( attached.getId() + " has been attached with deprecated code, "
-                        + "try to upgrade the responsible plugin" );
-                }
-
-                deployableArtifacts.add( attached );
-            }
-
-            deploy( executionEvent, deployableArtifacts, repo, retryFailedDeploymentCount );
-        }
-        catch ( ArtifactDeployerException e )
-        {
-            throw new IllegalArgumentException( e.getMessage(), e );
-        }
-    }
-
-    private void deploy( ExecutionEvent executionEvent, Collection<Artifact> artifacts,
-                         ArtifactRepository deploymentRepository, int retryFailedDeploymentCount )
-        throws ArtifactDeployerException
-    {
-
-        // for now retry means redeploy the complete artifacts collection
-        int retryFailedDeploymentCounter = Math.max( 1, Math.min( 10, retryFailedDeploymentCount ) );
-        ArtifactDeployerException exception = null;
-        for ( int count = 0; count < retryFailedDeploymentCounter; count++ )
-        {
-            try
-            {
-                if ( count > 0 )
-                {
-                    LOGGER.info( "Retrying deployment attempt " + ( count + 1 ) + " of "
-                        + retryFailedDeploymentCounter );
-                }
-
-                deployer.deploy( executionEvent.getSession().getProjectBuildingRequest(), deploymentRepository,
-                                 artifacts );
-                exception = null;
-                break;
-            }
-            catch ( ArtifactDeployerException e )
-            {
-                if ( count + 1 < retryFailedDeploymentCounter )
-                {
-                    LOGGER.warn( "Encountered issue during deployment: " + e.getLocalizedMessage() );
-                    LOGGER.debug( e.getMessage() );
-                }
-                if ( exception == null )
-                {
-                    exception = e;
-                }
-            }
-        }
-        if ( exception != null )
-        {
-            throw exception;
+            deployProject.deployProject( executionEvent.getSession().getProjectBuildingRequest(), deployRequest,
+                                         distributionManagementArtifactRepository );
         }
     }
 
