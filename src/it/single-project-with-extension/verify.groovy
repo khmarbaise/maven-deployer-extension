169d3d163bfc81fe493963451473e2e7b203dfe7
sparsick <sparsick@web.de>
2019-10-12 15:55:37 +0200

    add more robust test

--- src/it/single-project-with-extension/verify.groovy
+++ src/it/single-project-with-extension/verify.groovy
@@ -8,6 +8,8 @@ int deployerExtensionIndex  = 0
 int installingArtifactIndex  = 0
 int deployingArtifactIndex  = 0
 int uploadingToSnapshotIndex = 0
+int installPluginIndex = 0
+int deployPluginIndex = 0
 for(String line : lines ) {
     if(line.contains("org.apache.maven.plugins:maven-deploy-plugin:deploy has been deactivated.")) {
         deactivationOfDeployPluginIndex = lines.indexOf(line)
@@ -32,6 +34,14 @@ for(String line : lines ) {
     if(line.contains("Uploading to snapshots:")) {
         uploadingToSnapshotIndex = lines.indexOf(line)
     }
+
+    if(line.contains("--- maven-install-plugin:")) {
+        installPluginIndex = lines.indexOf(line)
+    }
+
+    if(line.contains("--- maven-deploy-plugin:")) {
+        deployPluginIndex = lines.indexOf(line)
+    }
 }
 
 assert deactivationOfDeployPluginIndex != 0
@@ -40,3 +50,5 @@ assert deployerExtensionIndex != 0
 assert installingArtifactIndex != 0 && installingArtifactIndex > deployerExtensionIndex
 assert deployingArtifactIndex !=0 && deployingArtifactIndex > deployerExtensionIndex
 assert uploadingToSnapshotIndex != 0
+assert installPluginIndex == 0
+assert deployPluginIndex == 0
